cf35746e837eff502a7465229fab785f7787ba4e
Jorge Sánchez <jorge.pachon@macalmpu1049.local>
2017-04-20 09:47:41 +0200

    Initial Commit

---
+++ IOSlogin2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSlogin2
+//
+//  Created by Jorge Pachón Sánchez on 20/4/17.
+//  Copyright © 2017 Jorge Pachón Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
